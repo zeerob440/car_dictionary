05f61d1226360b9832c1ad6d7243c9378bc9f199
zeerob440 <zeerob440@tutanota.com>
2026-03-06 14:25:04 -0500

    docs: root/car_dictionary.py declared, specifications for build declared car_dictionary.py

---
+++ car_dictionary.py
@@ -0,0 +1,3 @@
+# create a function that that sets make, model and color.
+# the function should create a dict that has the correct setting and contains the functions already created
+# the function should return a dict so users can assign any new car they want to it. 
