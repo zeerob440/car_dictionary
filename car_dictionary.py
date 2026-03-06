b91a3f34f3b332e5e0db38e772d5e82681b2cdb7
zeerob440 <zeerob440@tutanota.com>
2026-03-06 14:29:31 -0500

    update: car_dictionary.py| specifications, added additional spec

--- car_dictionary.py
+++ car_dictionary.py
@@ -1,3 +1,6 @@
-# create a function that that sets make, model and color.
-# the function should create a dict that has the correct setting and contains the functions already created
-# the function should return a dict so users can assign any new car they want to it. 
+''' 1 create a function that that sets make, model and color.
+    2 the function should create a dict that has the correct setting and contains the functions already created
+    3 the function should return a dict so users can assign any new car they want to it.
+    4 car types should be updated without affecting other cars 
+'''
+
