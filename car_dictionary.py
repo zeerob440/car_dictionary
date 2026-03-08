19a007d7e584f09bdd7ab738428f72908ec36719
zeerob440 <zeerob440@tutanota.com>
2026-03-08 13:02:18 -0400

    feat: car_dictionary.py| completed input validation, declared list cars, added make, model and year in add_car()

--- car_dictionary.py
+++ car_dictionary.py
@@ -8,19 +8,48 @@
     "Four Wheel and a Seat!" -Planes, Trains, and Automobiles- 1986
 '''
 # declarations 
-greet: str = 'Welcome to car_dictionary!'
+greet: str = 'Welcome to car_dictionary!\n'
+exit: str = 'Exiting program!\n'
 def add_car():
-    pass # build later, add functions to input car values make, model and year
+    make: str = input('Enter car make: ')
+    model: str = input('Enter car mode: ')
+
+    while True:
+        try:
+            year: int = int(input('Enter year: '))
+            break
+        except: ValueError
+        print ('Enter an integer!\n')
+    
+    return make, model, year
+
+
+#cars is a list of dictionaries. each dictionary element in the cars list is an individual car. 
+cars: list = []
 print(greet)
 
-selection: str = input('Would you like to enter a new car? Select "Y" or "N": ')
-# create input validation here
-if selection == 'y':
-    pass#build later
-elif selection == 'n':
-    pass# build later
-else: raise ValueError('Enter "Y" to enter an new car or "N" to not enter a new car.')
+while True:
+    selection: str = input('Would you like to enter a new car? Select "y" or "n": ')
+    #selection =  selection.lower()
+    if selection.lower() == 'y' or selection.lower() == 'n':
+        print('user input validated')
+        break
+    else:
+         print('Enter y or n')
 
-'''
-General plan for the build create a list of dictionaries, each dict in the list will be a new car, insert new cars in the dictionary
-'''
+if selection == 'y':
+    add_car()
+    # append cars list here: 
+else:
+    print(exit)
+
+
+
+
+
+          
+
+
+
+#General plan for the build create a list of dictionaries, each dict in the list will be a new car, insert new cars in the dictionary
+
